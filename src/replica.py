1cca8443809ebebb30be8ab46d79f54345184038
DesaulniersM <mdesaulniersm@gmail.com>
2024-11-07 17:50:41 -0700

    Redid replica.py

--- src/replica.py
+++ src/replica.py
@@ -1,10 +1,9 @@
-import select
-
 import jsonschema.exceptions
 from msg_utils import *
 import random
 import core
 import jsonschema
+import socket, threading
 
 JSON_SCHEMA = {
   "$schema": "http://json-schema.org/draft-04/schema#",
@@ -46,156 +45,124 @@ JSON_SCHEMA = {
 def validate_payload_schema(msg_payload):
     jsonschema.validate(instance=msg_payload, schema=JSON_SCHEMA)
 
-class Replica():
-    def __init__(self, node_id, replica_address_list, mode = 'sequential' ) -> None:
-        # If this class exists, then it must be the coordinator. So there is no flag.
-        self.consistency_mode = mode
-        self.replica_addresses = replica_address_list
+
+class Replica:
+    def __init__(self, replica_id, connections, mode):
+        self.replica_id = int(replica_id)
+        self.connections = connections      #list of (addr, port) tuples for all replicas
+        self.consistency_mode = mode        #string that describes mode
         self.coordinator_index = 0
         self.data = {}                      #dict to hold all post data, metadata, etc.
 
-        self.this_replica_id = node_id
-        self.me_socket = core.create_server(self.replica_addresses[node_id][0], self.replica_addresses[node_id][1], core.Modes.TCP)
-        self.connections = [None] * len(self.replica_addresses)
-        self.connect_to_replicas()        # includes our own socket and conections to every other replica
-
-        # self.elect_leader() #TODO
-    
+    #Sets coordinator flag
     @property
     def coordinator_flag(self):
-        return self.this_replica_id == self.coordinator_index
+        return self.replica_id == self.coordinator_index
+    
+    # Forward messages and wait to recevie ack
+    def forward_to_coordinator(self, message):
+        coord_host, coord_port = self.connections[0]
 
-    #POpulates list of sockets.
-    def connect_to_replicas(self):
-        if not self.coordinator_flag:
-            #wait for connection from coordinator
-        for replica in range(self.this_replica_id,len(self.replica_addresses)):
-            if replica != self.this_replica_id:
-                self.connections[replica] = core.create_client(self.replica_addresses[replica][0], self.replica_addresses[replica][1], core.Modes.TCP)
+        coord_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        coord_socket.connect((coord_host, coord_port))
+        coord_socket.sendall(message.encode('utf-8'))
+        return_message = coord_socket.recv(1000)
+        return str(return_message)
+    
+    # Forward message and don't wait to receive ack
+    def send_to_coordinator(self, message):
+        coord_host, coord_port = self.connections[0]
+        
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((coord_host, coord_port))
+            s.sendall(message.encode('utf-8'))
+
+   
+
+    #Function that handles incoming messages and acts on consistency logic. This links communication logic to consistency logic
+    def process_requests(self, conn, addr):
+        #TODO Here we will read with JSON library and parse message. We may get rid of this while loop
+        while True:
+            message = conn.recv(1024).decode('utf-8')
+            print(message)
+            if not message:
+                break
+            if message == 'post':
+                self.execute_post(conn, message)
+            elif message == 'choose':
+                self.execute_choose(conn, message)
+            elif message == 'write':
+                self.execute_write(conn)
+            elif message == 'read_data':
+                self.execute_read_data(conn)
             else:
-                self.connections[replica] = self.me_socket
+                print('unindentified message type')
+        conn.close()
 
-    def run_replica(self):
-        # Listen to all replica and client connections
-        readable, writable, _ = select.select(self.connections, [], [], 0.1)
+    
+    def execute_choose(self, conn, message):
+        if self.replica_id != 0:
+            #Forward this to the coordinator
+            return_message = self.forward_to_coordinator(message)
+            conn.sendall(return_message.encode('utf-8'))
 
-        for r in readable:
-            # Check if tcp or udp and handle accordingly
-            if r is self.me_socket: # ONLY RUNS FOR NEW CONNECTION!!!
-
-                conn, addr = self._tcp_read(r) # Accept connection
-
-                peers = [s.getpeername() for s in self.connections]
-
-                if addr not in peers:
-                    self.connections.append(conn)
-                else:
-                    # In case of EOF, remove the connection instantly
-                    for i,p in enumerate(peers):
-                        if p == addr:
-                            self.connections[i].close()
-                            self.connections.remove(self.connections[i])
-
-            # Saved TCP connections (mostly used in object oriented mode) that are sending will be handled here
-            else:
-                ret = self._tcp_read(r, accept=False)
-                if ret == 'KILL_CONNECTION': # Maybe just use the disconnect request here
-                    self.connections.remove(r)
-                    r.close() # hopefully this waits for the final message to clear the write queue, but not tragic if it doesn't.
-
-    # This function actually receives messages and processes them.
-    def _tcp_read(self, conn, accept=True):
-        if accept:
-            conn, addr = conn.accept()
-            conn.setblocking(1)
-            return (conn, addr)
-
-        # HANDLE REQUEST TYPE FIRST
-        msg = conn.recv(8) # We want int64, which will represent an integer value
-        request_type = unpack_msg(msg) # Always first byte of communication
-        print(f"* REQUEST TYPE: {request_type}")
-
-        nbytes_msg, = unpack('>Q', conn.recv(8)) # Need the comma during assignment to unpack tuple returned from unpack
-
-        msg = recvall(conn, nbytes_msg, chunk_size=512)
-
-        if request_type == REQUEST_TYPE.POST:
-            self.execute_POST(conn, msg)
-        elif request_type == REQUEST_TYPE.CHOOSE:
-            self.execute_CHOOSE(conn)
-        elif request_type == REQUEST_TYPE.READ:
-            self.execute_READ(conn)
-        elif request_type == REQUEST_TYPE.REPLY:
-            self.execute_REPLY(conn)
-        elif request_type == REQUEST_TYPE.DISCONNECT:
-            self.execute_DISCONNECT(conn)
-        elif request_type == REQUEST_TYPE.r_SYNC:
-            self.execute_SYNC(conn)
-        elif request_type == REQUEST_TYPE.r_GET_ID:
-            self.execute_GET_ID(conn)
-        elif request_type == REQUEST_TYPE.r_NOMINATE:
-            self.execute_NOMINATE(conn)
         else:
-            print(f"[!] Error: Client {conn} must attempt to resend its message. Invalid request type.")
-
-            try:
-                conn.send(b'[!] Error: Request Type must be one of those described in the Request_Type class... Please try again.')
-            except:
-                print(Warning('Client connection unexpectedly terminated.'))
-                return 'KILL_CONNECTION'
-
-    def execute_POST(self, conn, msg):
-        if self.coordinator_flag:
-            if self.consistency_mode == 'Sequential':
-                pass
-
-            elif self.consistency_mode == 'Quorum':
-                total_replicas_in_group = len(self.replica_addresses)
-                quorum_size = (total_replicas_in_group/2) + 1
-                replicas_to_write = random.sample(range(0, total_replicas_in_group), quorum_size)
-
-                for replica in replicas_to_write:
-                    print(f"Sending Write to replica number {replica}")
+            print("Hey, its me, coordinator, I'm choosing again.")
+            post_replicas = random.sample(range(0,len(self.connections)), 2)
+            for replica in post_replicas:
+                target_host, target_port = self.connections[replica]
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                    s.connect((target_host, target_port))
+                    s.sendall('read_data'.encode('utf-8'))
+                    action_message = str(s.recv(1000))
                 
-            elif self.consistency_mode == 'RYW':
-                pass
-            else:
-                print("Woah there. I don't recognize that mode")
+            conn.sendall(action_message.encode('utf-8'))
+
+    def execute_post(self, conn, message):
+        if self.replica_id != 0:
+            #Forward this to the coordinator
+            return_message = self.forward_to_coordinator(message)
+            conn.sendall(return_message.encode('utf-8'))
         else:
-            #Forward POST to coordinator
-            forward_message = pack_msg(msg)
-            #send message to coordinator
-            self.connections[self.coordinator_index].sendall(forward_message.encode('utf-8'))
+            print("Hey, its me, coordinator, I'm posting again.")
+            post_replicas = random.sample(range(0,len(self.connections)), 2)
+            for replica in post_replicas:
+                target_host, target_port = self.connections[replica]
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                    s.connect((target_host, target_port))
+                    s.sendall('write'.encode('utf-8'))
+                    action_message = str(s.recv(1000))
+            conn.sendall(action_message.encode('utf-8'))
 
-    def execute_CHOOSE(self, conn):
-        if self.coordinator_flag:
-            pass
-        else:
-            #Forward CHOOSE  to coordinator
-            pass
+    def execute_write(self, conn):
+        print('Received WRITE from coordinator')
+        # self.send_to_coordinator("Ack")
+        conn.sendall('ACK'.encode('utf-8'))
+        
+    def execute_read_data(self, conn):
+        print('Received read_data from coordinator')
+        # self.send_to_coordinator("Ack")
+        conn.sendall('ACK'.encode('utf-8'))
 
-    def execute_READ(self, conn):
-        if self.coordinator_flag:
-            pass
-        else:
-            #Forward READ to coordinator
-            pass
 
-    def execute_REPLY(self, conn):
-        if self.coordinator_flag:
-            pass
-        else:
-            #Forward REPLY to coordinator
-            pass 
+    def run_server(self):
+        while True:
+            # Block and accept new socket
+            conn, addr = self.server_socket.accept()
+            print(f"Node {self.replica_id} connected by {addr}")
+            # Pass new accepted socket connection to the process requests function
+            threading.Thread(target = self.process_requests, args=(conn,addr)).start()
 
-    def execute_DISCONNECT(self, conn):
-        pass
+    def run(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        my_host_name, my_host_port = self.connections[self.replica_id]
+        self.server_socket.bind((my_host_name, my_host_port))
+        self.server_socket.listen(5)
+        print(f"Node {self.replica_id} listening on {my_host_port}")
+        threading.Thread(target=self.run_server).start()
 
-    def execute_SYNC(self, conn):
-        pass
 
-    def execute_GET_ID(self, conn):
-        pass
+class Replica():
+    def __init__(self, node_id, replica_address_list, mode = 'sequential' ) -> None:
+        
 
-    def execute_NOMINATE(self, conn):
-        pass
